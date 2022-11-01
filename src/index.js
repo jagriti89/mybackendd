aac2980519a617d82dc755eeb9ba72790eba95dc
jagriti89 <jagritisinghpro@gmail.com>
2022-11-01 19:23:08 +0530

    change the code

--- src/index.js
+++ src/index.js
@@ -2,23 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment=require("moment");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jagriti:Jaggu123@cluster0.nf3nfa7.mongodb.net/jaggu", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
+//app.use (
+  //  function (req, res, next) {
+    //    console.log ("inside GLOBAL MW");
+       // next();
+  //}
+  //);
+  app.use(
+    function(req,res,next)
+{
+  const date=moment().format("YYYY-MM-DD hh:mm:ss")
+    const ipadd=req.socket.remoteAddress
+    console.log( date+" "+ipadd)
+    next();
+}
   );
 
 app.use('/', route);
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -19,8 +19,16 @@ const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
     next()
 }
+//const middassign = function(req,res,next)
+//{
+  //  const ipadd=req.socket.remoteAddress
+  //  console.log(ipadd)
+  //  next();
+//}
+
 
 module.exports.mid1= mid1
 module.exports.mid2= mid2
 module.exports.mid3= mid3
 module.exports.mid4= mid4
+//module.exports.middassign= middassign
