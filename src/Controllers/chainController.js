2ed7f0666a4318303f4ade775d02595e833ce3f2
rupamgupta-github <guptarupam.1020@gmail.com>
2022-12-07 22:16:49 +0530

    Data Sorted

--- src/Controllers/chainController.js
+++ src/Controllers/chainController.js
@@ -1,33 +1,25 @@
 const axios = require("axios");
-const BlockChain = require("../Models/chainModel");
-
-let response=null;
-exports.getBlockChain = async (req, res) => {
+const chainModel = require("../Models/chainModel");
+exports.getBlockChain = async function (req, res) {
   try {
-    response = await axios.get("https://api.coincap.io/v2/assets", {
-      headers: {
-        "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
-      },
+    let option = {
+      method: "get",
+      url: `https://api.coincap.io/v2/assets`,
+      
+    };
+    let result = await axios(option);
+    const { data } = await result.data;
+    data.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
+    await chainModel.deleteMany();
+    await data.forEach((element) => {
+      const result = new chainModel(element);
+      result.save();
     });
-  } catch (ex) {
-    response = null;
-    // error
-    console.log(ex);
-    res.json(ex);
-  }
-  if (response) {
-    // success
-    for (let i = 0; i < response.length; i++) {
-      const post = new BlockChain({
-        symbol: response[i]["symbol"],
-        name: response[i]["name"],
-        marketCapUsd: response[i]["marketCapUsd"],
-        priceUsd: response[i]["priceUsd"],
-      });
-      post.save();
-    }
-    const json = response.data;
-   // console.log(json);
-    res.status(200).send({ msg: json });
+
+    console.log(data);
+    res.status(200).send({ data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
   }
 };
